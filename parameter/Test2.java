1cc0b7ef4301e6ce8de1306b7da0303343199b7a
Jachen <81365742+Jachen99@users.noreply.github.com>
2022-10-12 13:57:28 +0800

    Update Test2.java

--- parameter/Test2.java
+++ parameter/Test2.java
@@ -5,16 +5,16 @@ public class Test2 {
 	public void f(){
 		a = 0;
 		b = 0;
-        str = "hello";
+        	str = "hello";
 		int[] c = {0};
 		g(b,c,str);
-		System.out.println(a + " " + b + " " + c[0] + "," + str);//
+		System.out.println(a + " " + b + " " + c[0] + "," + str);//101hello
 	}
 	public void g(int b, int[] c,String s){
 		a = 1;
 		b = 1;
 		c[0] = 1;
-        s = "world";
+        	s = "world";
 	}
 	public static void main(String[] args) {
 		Test t = new Test();
