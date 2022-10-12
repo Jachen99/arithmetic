091f83ccdc4f35526830e9c1a4698a5412b11deb
Jachen <81365742+Jachen99@users.noreply.github.com>
2022-10-12 14:01:46 +0800

    String对象不可改变

---
+++ parameter/Test3.java
@@ -0,0 +1,13 @@
+private static void change(String s,StringBuffer sb){
+		s = "aaaa";//字符串对象是不可变，一旦修改，就是新对象!!
+		sb.setLength(0);//先把sb里面的内容给清空了
+		sb.append("aaaa");//再拼接aaaa
+	}
+	
+	
+	public static void main(String[] args) {
+		String s = "bbbb";
+		StringBuffer sb = new StringBuffer("bbbb");
+		change(s,sb);
+		System.out.println(s+sb);//bbbbaaaa
+	}
