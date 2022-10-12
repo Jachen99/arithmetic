9ef3c37c172075324799e4601d0ce04f09f6ff5d
Jachen <81365742+Jachen99@users.noreply.github.com>
2022-10-12 13:56:36 +0800

    参数传递

---
+++ parameter/Test2.java
@@ -0,0 +1,23 @@
+public class Test2 {
+	int a;
+	int b;
+    String str;
+	public void f(){
+		a = 0;
+		b = 0;
+        str = "hello";
+		int[] c = {0};
+		g(b,c,str);
+		System.out.println(a + " " + b + " " + c[0] + "," + str);//
+	}
+	public void g(int b, int[] c,String s){
+		a = 1;
+		b = 1;
+		c[0] = 1;
+        s = "world";
+	}
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.f();
+	}
+}
